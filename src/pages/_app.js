af311b23a747a65c3df1b9f480324541831ccea4
folkmoz1 <folkmoz1@gmail.com>
2021-01-06 18:02:14 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,28 @@ import 'tailwindcss/tailwind.css'
 import '../../styles/global.css'
 import 'nprogress/nprogress.css'
 
+import { Router } from 'next/dist/client/router'
 import Header from "../components/Header";
 import Head from "next/head";
+import NProgress from 'nprogress'
+
+NProgress.configure({
+    showSpinner: false,
+    trickleRate: 0.1,
+    trickleSpeed: 800
+})
+
+Router.events.on('routerChangeStart', () => {
+    NProgress.start()
+})
+
+Router.events.on('routerChangeComplete', () => {
+    NProgress.done()
+})
+
+Router.events.on('routerChangeError', () => {
+    NProgress.done()
+})
 
 const MyApp = ({ Component, pageProps }) => {
 
