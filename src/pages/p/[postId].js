a86e5fbf3526b2f963a4c420834ef3565fc3f08d
folkmoz1 <folkmoz1@gmail.com>
2021-01-06 21:13:57 +0700

    Update [postId].js

--- src/pages/p/[postId].js
+++ src/pages/p/[postId].js
@@ -8,34 +8,65 @@ const Post_Page = ({ post }) => {
 
     const settings = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
     };
 
     return (
         <>
             <div className={'max-w-screen-xl p-8'}>
                 <div className="flex w-full">
-                    <div className="w-full md:w-1/2">
-                        <Slider {...settings}>
-                            {
-                                post.images.map(i => (
-                                    <Image
-                                        src={i.url}
-                                        width={400}
-                                        height={500}
-                                        objectFit={"cover"}
-                                    />
-
-                                ))
-                            }
-                        </Slider>
+                    <div className="w-full md:w-1/2 md:pt-20">
+                            <Slider {...settings}>
+                                {
+                                    post.images.map(i => (
+                                        <span key={i.public_id}>
+                                            <div className={'slider flex justify-center'}>
+                                                <Image
+                                                    src={i.url}
+                                                    width={350}
+                                                    height={450}
+                                                    objectFit={"cover"}
+                                                    alt={post.author}
+                                                    quality={100}
+                                                />
+                                                {/*<div>
+                                                    <img src={i.url} alt={post.author}  className={'object-cover'}/>
+                                                </div>*/}
+                                            </div>
+                                        </span>
+                                    ))
+                                }
+                            </Slider>
                     </div>
                     <div className="w-full md:w-1/2"></div>
                 </div>
             </div>
+            <style jsx global>{`
+              img[alt="${post.author}"] {
+                  width: 100%;
+                  height: 100%;
+              }
+              
+              .slider div{
+                 box-shadow: 0 0 6px rgba(0, 0, 0, .6);
+                 border-radius: 16px;
+                 overflow: hidden;
+                 height: 450px;
+                 width: 350px;
+                 
+              }
+              
+              .slick-dots li:hover button:before {
+                color: red!important;
+              }
+              
+              .slick-active button:before {
+                color: red!important;
+              }
+            `}</style>
         </>
     )
 }
